01c2af743d29d72dae785aa8da115426695c39cb
Nicholas Prussen <59841031+nicholasprussen@users.noreply.github.com>
2024-02-05 06:54:18 +0000

    Added cardinal direction animations

--- index.js
+++ index.js
@@ -9,20 +9,30 @@ let numHigh;
 let marginX;
 let marginY;
 let cells;
-let cellSize;
-let mouseX;
-let mouseY;
+let cellSize = 75;
+let mouseX = 999999;
+let mouseY = 999999;
+let chanceToMove = 0.15;
+//let chanceToMove = 0;
+let numFrames = 0;
+
+let north = 0;
+let east = 1;
+let south = 2;
+let west = 3;
+
+let changePerFrame = 0.12;
+let changePerFrameMargin = 15;
+let framesToFullSpeed = 1000;
 
 function init() {
     cvWidth = cv.width;
     cvHeight = cv.height;
     cells = [];
-    cellSize = 75;
     numWidth =  Math.floor(cvWidth / cellSize);
     numHigh = Math.floor(cvHeight / cellSize);
     marginY = (cvHeight - (numHigh * cellSize)) / 2;
     marginX = (cvWidth - (numWidth * cellSize)) / 2;
-    console.log(cvHeight, cvWidth, marginX, marginY)
     buildCells();
     animate();
 }
@@ -39,32 +49,86 @@ function resize() {
     buildCells();
 }
 
-function calcDistance(x1, y1, x2, y2) {
-    const X = x2 - x1;
-    const Y = y2 - y1;
-    return Math.sqrt(X * X + Y * Y );
-}
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * 10);
-}
-
 class Cell {
-    number = getRandomNumber();
+    number;
     x;
     y;
     numX;
     numY;
     color = "000000";
+    direction;
+    timeToWait;
 
     constructor(x, y, color) {
         this.color = color;
         this.x = x;
         this.y = y;
-        this.updateNumPos();
+        this.centerNums();
+        this.number = this.getRandomNumber();
+        this.direction = this.getInitialDirection();
+    }
+
+    getRandomNumber() {
+        return Math.floor(Math.random() * 10);
     }
 
     updateNumPos() {
+        //console.log(numFrames, this.direction)
+        //console.log(this.timeToWait, numFrames)
+        if (this.timeToWait > numFrames) { //Return if we don't want you to move yet
+            //console.log(this.timeToWait, numFrames)
+            return;
+        }
+        let speed;
+        const framesSinceAnimStarted = numFrames - this.timeToWait;
+        if (framesSinceAnimStarted > 0 && framesSinceAnimStarted < framesToFullSpeed) {
+            speed = changePerFrame * (framesSinceAnimStarted / framesToFullSpeed);
+        } else {
+            speed = changePerFrame;
+        }
+
+        let outside;
+        switch(this.direction) {
+            case 0: //North
+                outside = this.numY <= this.y + changePerFrameMargin;
+                if (!outside) {
+                    this.numY -= speed;
+                } else {
+                    this.direction = 2;
+                    this.numY += speed;
+                }
+                break;
+            case 2: //South
+                outside = this.numY >= this.y + (cellSize - changePerFrameMargin);
+                if (!outside) {
+                    this.numY += speed;
+                } else {
+                    this.direction = 0;
+                    this.numY -= speed;
+                }
+                break;
+            case 1: //West
+                outside = this.numX <= this.x + changePerFrameMargin;
+                if (!outside) {
+                    this.numX -= speed;
+                } else {
+                    this.direction = 3;
+                    this.numX += speed;
+                }
+                break;
+            case 3: //East
+                outside = this.numX >= this.x + (cellSize - changePerFrameMargin);
+                if (!outside) {
+                    this.numX += speed;
+                } else {
+                    this.direction = 1;
+                    this.numX -= speed;
+                }
+                break;
+        }
+    }
+
+    centerNums() {
         this.numX = this.x + (cellSize / 2);
         this.numY = this.y + (cellSize / 2);
     }
@@ -82,16 +146,31 @@ class Cell {
         return `${fontSize}px helvetica`
     }
 
+    calcDistance(x1, y1, x2, y2) {
+        const X = x2 - x1;
+        const Y = y2 - y1;
+        return Math.sqrt(X * X + Y * Y );
+    }
+
+    getInitialDirection() {
+        const chance = Math.random();
+        if (chance < chanceToMove) {
+            const direction = Math.floor(Math.random() * 4);
+            this.timeToWait = Math.floor(Math.random() * 2000);
+            return direction;
+        }
+    }
+
     draw() {
-        //ctx.fillStyle = this.color;
+        if (this.direction !== null && this.direction !== undefined) {
+            this.updateNumPos();
+        }
         ctx.fillStyle = "transparent";
         ctx.fillRect(this.x, this.y, cellSize, cellSize);
         ctx.textAlign="center"; 
         ctx.textBaseline = "middle";
         ctx.fillStyle = "#aaf3fc"
-        //ctx.filter = "blur(1px)";
-        const distance = calcDistance(this.numX, this.numY, mouseX, mouseY);
-        //console.log(distance)
+        const distance = this.calcDistance(this.numX, this.numY, mouseX, mouseY);
         ctx.font = this.getFontSize(distance);
         ctx.fillText(this.number.toString(), this.numX, this.numY);
     }
@@ -100,7 +179,6 @@ class Cell {
 function mouseMove(event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
-    //console.log(mouseX, mouseY);
 }
 
 function buildCells() {
@@ -112,52 +190,41 @@ function buildCells() {
             let xLoc = marginX + (x * cellSize);
             let yLoc = marginY + (y * cellSize);
             const existing = cellsCopy[index];
-            //console.log(existing, cellsCopy);
             if (existing !== null && existing !== undefined) {
                 cells.push(cellsCopy[index]);
                 cells[index].x = xLoc;
                 cells[index].y = yLoc;
-                cells[index].updateNumPos();
+                cells[index].centerNums();
             } else {
                 cells.push(new Cell(xLoc, yLoc, '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')));
             }
             index++;
         }
     }
-    //console.log(cells)
 }
 
 function animate() {
-  //ctx.clearRect(0, 0, cvWidth, cvHeight);
-  // call again next time we can draw
-  // clear canvas
-  //ctx.fillRect()
-  //ctx.clearRect(0, 0, cvWidth, cvHeight);
-  //ctx.beginPath();
   requestAnimationFrame(animate);
+  //console.log(numFrames);
   ctx.fillStyle = "#000";
+  
   ctx.fillRect(0, 0, cvWidth, cvHeight);
   cells.forEach(cell => cell.draw());
-//   cells.forEach(function(o) {
-//     ctx.fillStyle = o.color;
-//     ctx.fillRect(o.x, o.y, cellSize, cellSize);
-//   });
+  numFrames++;
 }
 
-//animate();
+function debounce(callback, delay) {
+    let timer
+    return function() {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        callback();
+      }, delay)
+    }
+  }
 
 window.addEventListener('load', init);
 
-window.addEventListener('resize', resize)
+window.addEventListener('resize', debounce(resize, 300))
 
 window.addEventListener('mousemove', mouseMove)
-
-// click handler to add random rects
-// window.addEventListener('click', function() {
-//   addRandRect();
-// });
-
-// function addRandRect() {
-//   let randColor = Math.random() > 0.5 ? 'blue' : 'red';
-//   everyObject.push([Math.random() * cvWidth, Math.random() * cvHeight, 10 + Math.random() * 40, 10 + Math.random() * 40, randColor]);
-// }
