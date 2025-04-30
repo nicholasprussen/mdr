9e9903b296dae878846cc8813719b4a6e280ee08
nicholasprussen <nicholasprussen@u.boisestate.edu>
2025-04-29 22:45:05 -0600

    Added thicker startup animation

--- src/startup-animation.ts
+++ src/startup-animation.ts
@@ -16,10 +16,12 @@ export class StartupAnimation {
         container.style.display = 'flex';
 
         let animationContainer = document.getElementById('startup-animation') as HTMLDivElement;
-        fetch('assets/img/lumon-logo.svg')
+        fetch('assets/img/lumon-logo-thicker-2.svg')
             .then(response => response.text())
             .then(svgText => {
-                animationContainer.innerHTML = svgText
+                animationContainer.innerHTML = svgText;
+                (document.getElementById('startup-overlay') as HTMLDivElement).style.display = 'none';
+                (document.getElementById('blinking-cursor') as HTMLDivElement).style.display = 'none';
                 setTimeout(() => {
                     (document.getElementById('startup-overlay-container') as HTMLDivElement).style.display = 'none';
                     this.completionPromise?.();
